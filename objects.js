d402f04462096a2901dbca78367555f66c8dd7bd
Tommi Tuura <tommi.tuura@gmail.com>
2012-12-13 21:24:39 +0200

    changed bounds to do even more aggressive pre-pruning

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@
 // gameplay/animation/etc. objects
 
 sf.objects = {
-    WorldObject: {
+    GameObject: {
         moveBy: function(x, y) {
             if (this.movable) {
                 this.prevpos.x = this.pos.x;
@@ -18,12 +18,33 @@ sf.objects = {
             this.bounds.right = this.pos.x - (this.outerwidth/2);
             this.bounds.bottom = this.pos.y + (this.outerheight/2);
             this.bounds.left = this.pos.x + (this.outerwidth/2);
+            this.prevbounds.top = this.prevpos.y - (this.outerheight/2);
+            this.prevbounds.right = this.prevpos.x - (this.outerwidth/2);
+            this.prevbounds.bottom = this.prevpos.y + (this.outerheight/2);
+            this.prevbounds.left = this.prevpos.x + (this.outerwidth/2);
+            // For some aggressive optimization of collision detection...
+            this.movebounds.top = this.bounds.top < this.prevbounds.top ? this.bounds.top : this.prevbounds.top;
+            this.movebounds.left = this.bounds.left < this.prevbounds.left ? this.bounds.left : this.prevbounds.left;
+            this.movebounds.bottom = this.bounds.bottom > this.prevbounds.bottom ? this.bounds.bottom : this.prevbounds.bottom;
+            this.movebounds.right = this.bounds.right > this.prevbounds.right ? this.bounds.right : this.prevbounds.right;
+            
         },
         crudeCollision: function(that) { // This collision detection is for simple cases. Remember to run calculateBounds for both this and that!
-            if (this.bounds.right < that.bounds.left && 
-                that.bounds.right < this.bounds.left && 
-                this.bounds.top < that.bounds.bottom && 
-                that.bounds.top < this.bounds.bottom) {
+        /*
+            var thistop = this.bounds.top < this.prevbounds.top ? this.bounds.top : this.prevbounds.top;
+            var thisleft = this.bounds.left < this.prevbounds.left ? this.bounds.left : this.prevbounds.left;
+            var thisbottom = this.bounds.bottom > this.prevbounds.bottom ? this.bounds.bottom : this.prevbounds.bottom;
+            var thisright = this.bounds.right > this.prevbounds.right ? this.bounds.right : this.prevbounds.right;
+            
+            var thattop = that.bounds.top < that.prevbounds.top ? that.bounds.top : that.prevbounds.top;
+            var thatleft = that.bounds.left < that.prevbounds.left ? that.bounds.left : that.prevbounds.left;
+            var thatbottom = that.bounds.bottom > that.prevbounds.bottom ? that.bounds.bottom : that.prevbounds.bottom;
+            var thatright = that.bounds.right > that.prevbounds.right ? that.bounds.right : that.prevbounds.right;
+          */  
+            if (this.movebounds.right < that.movebounds.left && 
+                that.movebounds.right < this.movebounds.left && 
+                this.movebounds.top < that.movebounds.bottom && 
+                that.movebounds.top < this.movebounds.bottom) {
                 return true;
             }
             else {
@@ -41,6 +62,17 @@ sf.objects = {
                 return false;
             }
         },
+        init: function(posX, posY, movable, outerwidth, outerheight) {
+            this.pos = {x: posX, y: posY };
+            this.prevpos = {x: posX, y: posY };
+            this.movable = movable;
+            this.outerwidth = outerwidth;
+            this.outerheight= outerheight;
+            this.bounds = {};
+            this.prevbounds = {};
+            this.movebounds = {};
+            this.calculateBounds();
+        },
         draw: function(context, color) {
             if (typeof color === 'undefined') {var color='rgb(0,0,0)';}
             if (!this.sprite) {
@@ -53,27 +85,15 @@ sf.objects = {
         }    
     },
     DummyObject: function(posX, posY, movable) {
-        this.pos = {x: posX, y: posY };
-        this.prevpos = {x: posX, y: posY };
-        this.movable = movable;
-        this.outerwidth = 10;
-        this.outerheight= 10;
-        this.bounds = {};
-        this.calculateBounds();
+        this.init(posX, posY, movable, 10, 10);
     },
     PlayerObject: function(posX, posY, movable) {
-        this.pos = {x: posX, y: posY };
-        this.prevpos = {x: posX, y: posY };
-        this.movable = movable;
-        this.outerwidth = 10;
-        this.outerheight= 10;
-        this.bounds = {};
-        this.calculateBounds();
+        this.init(posX, posY, movable, 10, 10);
         this.draw = function(context) {
             context.fillStyle='rgb(0,255,0)';
             context.fillRect(this.bounds.top, this.bounds.left, this.outerwidth, this.outerheight);
         }
     }
 } 
-sf.objects.DummyObject.prototype = sf.objects.WorldObject;
-sf.objects.PlayerObject.prototype = sf.objects.WorldObject;
+sf.objects.DummyObject.prototype = sf.objects.GameObject;
+sf.objects.PlayerObject.prototype = sf.objects.GameObject;

a82b81c1f5c33e53d3c8a3e71e09cc916f17dc02
Tommi Tuura <tommi.tuura@gmail.com>
2012-12-15 20:50:27 +0200

    moved stuff around in directories.

--- minify.sh
+++ minify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cat setup.js > spich-firish.js
-cat controls.js >> spich-firish.js
-cat objects.js >> spich-firish.js
-cat main.js >> spich-firish.js
-cat test.js >> spich-firish.js
+cat js/setup.js > spich-firish.js
+cat js/inputs/*.js >> spich-firish.js
+cat js/models/*.js >> spich-firish.js
+cat js/main.js >> spich-firish.js
+cat js/test.js >> spich-firish.js
 java -jar ../yuicompressor-2.4.7.jar spich-firish.js > spich-firish.min.js
 rm spich-firish.js

f853393bd78f211c09d922eb15a2930684c7101d
Tommi Tuura <tommi.tuura@gmail.com>
2013-01-08 00:40:06 +0200

    state switching, first version... beware for speed hit

--- js/engine.js
+++ js/engine.js
@@ -12,7 +12,19 @@ window.requestAnimFrame = (function(){
 })();
 
 sf.engine = (function() {
-    var draw = function() {
+    var state = 'START_SCREEN';
+    var draw = function () {
+        if (state === 'GAME_SCREEN') {
+            gameDraw();
+        } else if (state === 'START_SCREEN') {
+            startDraw();
+        } else if (state === 'SCORE_SCREEN') {
+            scoreDraw();
+        } else if (state === 'SCOREINPUT_SCREEN') {
+            scoreInputDraw();
+        }
+    }
+    var gameDraw = function() {
         sf.setup.context.fillStyle="rgb(255,255,255)";
         sf.setup.context.fillRect(0, 0, 800, 800);
         
@@ -22,11 +34,47 @@ sf.engine = (function() {
         sf.world.player.draw(sf.setup.context)
         
     }
+    var startDraw = function() {
+        sf.setup.context.fillStyle="rgb(255,255,255)";
+        sf.setup.context.fillRect(0, 0, 800, 800);
+        
+        sf.setup.context.font = '20px Arial, Helvetica, Sans-serif';
+        sf.setup.context.fillStyle = 'rgb(0, 0, 0)';
+        sf.setup.context.fillText("Start screen!", 30, 30);
+    }
+    var scoreDraw = function() {
+        sf.setup.context.fillStyle="rgb(255,255,255)";
+        sf.setup.context.fillRect(0, 0, 800, 800);
+        
+        sf.setup.context.font = '20px Arial, Helvetica, Sans-serif';
+        sf.setup.context.fillStyle = 'rgb(0, 0, 0)';
+        sf.setup.context.fillText("Score screen!", 30, 30);
+    }
+    var scoreInputDraw = function() {
+        sf.setup.context.fillStyle="rgb(255,255,255)";
+        sf.setup.context.fillRect(0, 0, 800, 800);
+        
+        sf.setup.context.font = '20px Arial, Helvetica, Sans-serif';
+        sf.setup.context.fillStyle = 'rgb(0, 0, 0)';
+        sf.setup.context.fillText("Score Input screen!", 30, 30);
+    }
     var tick = function() {
         sf.engine.draw();
         requestAnimFrame(sf.engine.tick);
     }
+    var stateSwitch = function(switchto) {
+        if (switchto === 'SCORE_SCREEN') {
+            state = 'SCORE_SCREEN';
+        } else if (switchto === 'SCOREINPUT_SCREEN') {
+            state = 'SCOREINPUT_SCREEN';
+        } else if (switchto === 'GAME_SCREEN') {
+            state = 'GAME_SCREEN';
+        } else {
+            state = 'START_SCREEN';
+        }
+    }
     return {
+        state: stateSwitch,
         draw: draw,
         tick: tick
     }
--- minify.sh
+++ minify.sh
@@ -4,6 +4,7 @@ cat js/inputs/*.js >> spich-firish.js
 cat js/models/gameobject.js >> spich-firish.js
 cat js/models/dummyobject.js >> spich-firish.js
 cat js/models/playerobject.js >> spich-firish.js
+cat js/engine.js >> spich-firish.js
 cat js/main.js >> spich-firish.js
 cat js/test.js >> spich-firish.js
 java -jar ../yuicompressor-2.4.7.jar spich-firish.js > spich-firish.min.js

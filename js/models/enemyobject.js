855ddaf49307bf0d60f0ce26366511d63c0828f4
Tommi Tuura <tommi.tuura@gmail.com>
2013-01-15 04:29:31 +0200

    cosmetic check for enemyobject.js

--- js/models/enemyobject.js
+++ js/models/enemyobject.js
@@ -2,6 +2,7 @@ sf.objects.EnemyObject = function(posX, posY, movable) {
     this.init(posX, posY, movable, 10, 10);
     this.life = 1;
     this.hit = function(dmg) {
+        if (typeof dmg === 'undefined') var dmg = 1;
         this.life = this.life - dmg;
     }
 };

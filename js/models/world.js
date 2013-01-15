35e943e327f9de0ec414fa3ef51b76c88b45c920
Tommi Tuura <tommi.tuura@gmail.com>
2013-01-15 04:08:11 +0200

    levels are now fetched... parsing still incomplete

--- js/models/world.js
+++ js/models/world.js
@@ -1,16 +1,52 @@
-sf.world = {
-    enemies: [],
-    enemybullets: [],
-    playerbullets: [],
-    terrain: [],
-    player: null,
-    warehouse: {
-        enemies: [],
-        enemybullets: [],
-        playerbullets: [],
-        terrain: []
-    },
-    build: (function() {
+sf.levels = (function() {
+    var parsedLevels = 0;
+    
+    var levels = [];
+    
+    
+    var initLevel = function(levelFile, levelIndex) {
+        $.getJSON(levelFile, function(oneLevelData) {
+            sf.debug(levelFile, levelIndex, oneLevelData);
+            levels[levelIndex].data = oneLevelData;
+            levels[levelIndex].parsed = true;
+            var isReady = true;
+            for (var i=0; i<levels.length;i++) {
+                if (!levels[i].parsed) {
+                    isReady = false;
+                }
+            }
+            if (isReady) sf.debug('Ready now.');
+            sf.setup.readyToRun = isReady
+        });
+    }
+    
+    var initWorldCallback = function(data) {
+        levels = data;
+        for (var i=0; i<levels.length; i++) {
+            levels[i].parsed = false;
+            initLevel(levels[i].file, i);
+        }
+        //console.log('initWorldCallback finishing... levels:', levels);
+    }
+    
+    // This function takes the filename which has the json files of actual levels... 
+    var initWorld = function(mainfile) {
+        $.getJSON(mainfile, initWorldCallback);
+    }
+    
+    // This function returns the number of levels available
+    var levelCount = function() {
+        return levels.length;
+    }
+    
+    // This returns the asked level.
+    var getLevel = function(index) {
         
-    })()
-};
+    }
+    return {
+        initWorld: initWorld,
+        levelCount: levelCount,
+        getLevel: getLevel
+    }
+})();
+
